5c688c767bd4566bc5deed8026a54fdcb11102f3
HarshilSolanki5 <harshil5.solanki@gmail.com>
2025-09-02 17:17:39 +0530

    Work on Login

--- frontend/Expense-tracker/src/App.jsx
+++ frontend/Expense-tracker/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Login from './pages/Auth/login';
+import SignUp from './pages/Auth/signUp';
+import Home from './pages/Dashboard/home';
+import Income from './pages/Dashboard/income';
+import Expense from './pages/Dashboard/expense';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localstiorage.getItem("token");
+  return isAuthenticated ? ( 
+  <Navigate to="/dashboard" /> 
+  ) : ( 
+  <Navigate to="/login" />
+  );
+};
